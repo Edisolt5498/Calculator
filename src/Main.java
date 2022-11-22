649c468f5f2fee2282115d273abb6b80a40c5ffa
Edisolt5498 <edisolt5498@gmail.com>
2022-11-22 10:03:47 +0300

    looks little better

--- src/Main.java
+++ src/Main.java
@@ -39,53 +39,58 @@ public class Main {
 
             if ((InputUser.charAt(i)) == '+' || (InputUser.charAt(i)) == '-' || (InputUser.charAt(i)) == '*' || (InputUser.charAt(i)) == '/') {
 
-                char operator = InputUser.charAt(i);                                                                                                             // проверка на наличие знака и сохранение знака и потенциальных чисел
+                char operator = InputUser.charAt(i); // проверка на наличие знака и сохранение знака и потенциальных чисел
                 String num1 = InputUser.substring(0, i);
                 String num2 = InputUser.substring(i + 1, InputUser.length());
 
-                if ((checker(new String[]{num1, num2}, BookRoman)) == 1) {                                                                                       // проверка на арабские и на ошибки
-                    return ArabCalc(new String[]{num1, num2}, operator);                                                                                         // калькулятор для арабских чисел
+                if ((checker(new String[]{num1, num2}, BookRoman)) == 1) { // проверка на арабские и на ошибки (return = 1 - араб. числа; 2 - рим. числа;
+                    return ArabCalc(new String[]{num1, num2}, operator); // калькулятор для арабских чисел               3 - разные; 4 - ни то ни другое)
                 } else if ((checker(new String[]{num1, num2}, BookRoman)) == 2) {
-                    return RomanCalc(new String[]{num1, num2}, BookRoman, operator);                                                                            // калькулятор для римских чисел
+                    return RomanCalc(new String[]{num1, num2}, BookRoman, operator); // калькулятор для римских чисел
                 } else if ((checker(new String[]{num1, num2}, BookRoman)) == 3) {
-                    throw new InputMismatchException("Тут слишком разное, давай как я просил...Неправильный ввод...");
+                    throw new InputMismatchException("Тут слишком разное, давай как я просил...Неправильный ввод..."); // ловим 3 и 4 ошибки
                 } else {
                     throw new InputMismatchException ("Мне кажется, или тут нет никаких чисел...или я не знаю...Неправильный ввод...");
                 }
+
             }
+
         }
 
-        throw new InputMismatchException("Где знак?...Неправильный ввод...");                                                 //ГДЕ ОН?
-        }
+        throw new InputMismatchException("Где знак?...Неправильный ввод..."); //ГДЕ ОН?. ловим отсутствие знака
+
+    }
 
     public static int checker(String[] numbs, String[] Book) {
 
         byte check = 0;
         //boolean Arab = false;
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) { // недопустить разные системы счета
+
             if (numbs[i].contentEquals("0") || numbs[i].contentEquals("1") || numbs[i].contentEquals("2")
                     || numbs[i].contentEquals("3") || numbs[i].contentEquals("4") || numbs[i].contentEquals("5")
                     || numbs[i].contentEquals("6") || numbs[i].contentEquals("7") || numbs[i].contentEquals("8")
                     || numbs[i].contentEquals("9") || numbs[i].contentEquals("10")) {
                 if (check == 2) {
                     check = 3;
-                }else {
-                    check = 1;}
-
+                } else {
+                    check = 1;
+                }
             } else if (numbs[i].contentEquals(Book[0]) || numbs[i].contentEquals(Book[1]) || numbs[i].contentEquals(Book[2])
                     || numbs[i].contentEquals(Book[3]) || numbs[i].contentEquals(Book[4]) || numbs[i].contentEquals(Book[5])
                     || numbs[i].contentEquals(Book[6]) || numbs[i].contentEquals(Book[7]) || numbs[i].contentEquals(Book[8])
                     || numbs[i].contentEquals(Book[9]) || numbs[i].contentEquals(Book[10])) {
                 if (check == 1) {
                     check = 3;
-                }else {
-                    check = 2;}
-
+                } else {
+                    check = 2;
+                }
             } else {
                 check = 4;
                 break;
             }
+
         }
 
         return check;
@@ -130,6 +135,7 @@ public class Main {
         int num1 = Integer.parseInt(numbs[0]);
         int num2 = Integer.parseInt(numbs[1]);
         int sum = 0;
+
         switch (operator) {
             case '+' -> sum = num1 + num2;
             case '-' -> sum = num1 - num2;
